ed5afb0bf4e75915b553186cbc9d814b451c46c7
moyiz <8603313+moyiz@users.noreply.github.com>
2024-06-15 18:05:41 +0300

    fix(ui): set UI buffer options on the buffer itself, ensure buffer exists before write.

--- lua/git-dev/ui.lua
+++ lua/git-dev/ui.lua
@@ -1,17 +1,31 @@
+---Create a new scratch and unlisted buffer and return its handle.
 local function _create_buffer()
-  local _set_option = function(name, value)
-    vim.api.nvim_set_option_value(name, value, { scope = "local" })
-  end
-
   local buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_name(buf, "git-dev#" .. buf)
-  _set_option("buftype", "nofile")
-  _set_option("swapfile", false)
-  _set_option("bufhidden", "wipe")
-  _set_option("readonly", true)
-  _set_option("modifiable", false)
-  _set_option("signcolumn", "no")
+  -- Scratch buffer
+  vim.api.nvim_set_option_value("buftype", "nofile", { buf = buf })
+  vim.api.nvim_set_option_value("bufhidden", "hide", { buf = buf })
+  vim.api.nvim_set_option_value("swapfile", false, { buf = buf })
+  -- Unlisted
+  vim.api.nvim_set_option_value("buflisted", false, { buf = buf })
   return buf
+end
+
+local function _lock_buffer(buf)
+  vim.api.nvim_set_option_value("readonly", true, { buf = buf })
+  vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
+end
+
+local function _unlock_buffer(buf)
+  vim.api.nvim_set_option_value("readonly", false, { buf = buf })
+  vim.api.nvim_set_option_value("modifiable", true, { buf = buf })
+end
+
+---Create a window with preset options.
+local function _create_window(buf, config)
+  local win = vim.api.nvim_open_win(buf, false, config)
+  vim.api.nvim_set_option_value("signcolumn", "no", { win = win })
+  return win
 end
 
 ---@alias win_config vim.api.keyset.win_config
@@ -25,13 +39,20 @@ local UI = {}
 function UI:init(o)
   setmetatable(o or {}, self)
   self.__index = self
-  self.buffer = self.buffer or _create_buffer()
+  self.buffer = nil
   self.window = nil
   self.win_config = o.win_config
   return self
 end
 
----Show window.
+---Ensure that `self.buffer` is a valid buffer handle.
+function UI:_ensure_buffer_valid()
+  if not self.buffer or not vim.api.nvim_buf_is_valid(self.buffer) then
+    self.buffer = _create_buffer()
+  end
+end
+
+---Show UI window.
 ---Closes previous window if exists.
 ---@param win_config? win_config
 function UI:show(win_config)
@@ -39,21 +60,24 @@ function UI:show(win_config)
   if self.window ~= nil then
     vim.api.nvim_win_close(self.window, true)
   end
-  self.window = vim.api.nvim_open_win(self.buffer, false, config)
+  self:_ensure_buffer_valid()
+  self.window = _create_window(self.buffer, config)
   self:redraw()
   return self
 end
 
 ---Redraws window and sets cursor to the last line.
 function UI:redraw()
+  self:_ensure_buffer_valid()
   local row = vim.api.nvim_buf_line_count(self.buffer)
   -- Follow text in buffer
   vim.api.nvim_win_set_cursor(self.window, { row, 1 })
   vim.cmd.redraw()
 end
 
----Prints given arguments into the current buffer.
+--A `vim.print` like function that appends to `self.buffer`.
 function UI:print(...)
+  -- Transform arguments into a space delimited string and append '\n'.
   local args = { ... }
   local s = ""
   for i = 1, #args do
@@ -64,8 +88,12 @@ function UI:print(...)
     end
   end
   s = s .. "\n"
+
+  -- Schedule the write.
   vim.schedule(function()
+    self:_ensure_buffer_valid()
     local lines = vim.api.nvim_buf_line_count(self.buffer) - 1
+    _unlock_buffer(self.buffer)
     vim.api.nvim_buf_set_text(
       self.buffer,
       lines,
@@ -74,6 +102,7 @@ function UI:print(...)
       0,
       vim.split(s, "\n")
     )
+    _lock_buffer(self.buffer)
     self:redraw()
   end)
   return self
