938400da5462274987df4ba49a5b72d28e90e71a
moyiz <8603313+moyiz@users.noreply.github.com>
2024-06-21 21:44:51 +0300

    feat(history): maintain a simple DB to log opened repositories

---
+++ tests/store_spec.lua
@@ -0,0 +1,43 @@
+local function test_sanity()
+  local t = dofile("./tests/lib.lua").Session:init {}
+  local Store = require "git-dev.store"
+  local st = Store:init {}
+
+  local keys = {}
+  for i = 1, 30 do
+    keys[i] = st:add(i)
+    -- Ensure `update_time` will differ between records for consistent tests.
+    vim.uv.sleep(1)
+  end
+
+  t:assert(st:total() == 30, "Number of records.")
+  t:assert(
+    vim.deep_equal(st:least_recent(3), { keys[1], keys[2], keys[3] }),
+    "Least recent items."
+  )
+
+  for i = 1, 10 do
+    st:remove(keys[i])
+  end
+
+  t:assert(st:total() == 20, "Number of records.")
+  t:assert(
+    vim.deep_equal(st:least_recent(3), { keys[11], keys[12], keys[13] }),
+    "Least recent items."
+  )
+
+  t:assert(st:get(keys[2]) == nil, "Non-existing key.")
+  t:assert(st:get(keys[20]) == 20, "Existing key.")
+
+  st:purge()
+
+  t:assert(st:total() == 0, "Purging store.")
+
+  return t.failed == 0
+end
+
+local T = {}
+T.run = function()
+  return test_sanity()
+end
+return T
